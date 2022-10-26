8f2464dd7aa51e81b04f302d465d3633717a94c9
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2022-10-26 12:42:24 +0100

    latest changes

--- js/index.js
+++ js/index.js
@@ -4,16 +4,17 @@
 // const element = document.getElementById(id)
 // const elementTwo = document.querySelector("#id")
 
-function deleteTodo() {
-  alert("i am deleteing");
+function deleteTodo(eventObject) {
+  const buttonClicked = eventObject.target;
+  const todoItem = buttonClicked.parentNode.parentNode.parentNode;
+  todoItem.remove();
 }
-
+ 
 function addListenersToDeleteButtons() {
   // select all the buttons with .delete__btn class.
   //remember querySelectorAll return an array (NodeList)
   // only the last element in the array is new. every other element as been existing before;
   const buttonCollection = document.querySelectorAll(".delete__btn");
-  console.log(buttonCollection);
   // select the last element in the array;
   const lastButton = buttonCollection[buttonCollection.length - 1];
   lastButton.addEventListener("click", deleteTodo);
@@ -24,16 +25,19 @@ function showTodoOnTheScreen(todo) {
   const li = document.createElement("li");
   li.classList.add("todo__section--todoItem");
 
-  li.innerHTML = `<div>
+  li.innerHTML = `<div class="Rethabile">
   <input class="todo__checkbox" type="checkbox" />
   <span class="todo__text"> ${todo} </span>
+
   <div class="todo__actions--wrapper">
     <button id="edit_btn" class="todo__actions edit__btn">
       edit
     </button>
+
     <button id="delete_btn" class="todo__actions delete__btn">
       delete
     </button>
+
   </div>
 </div>`;
 
@@ -46,7 +50,16 @@ function todoFormSumbitHandler(eventObject) {
   const inputField = document.querySelector("#todo__input");
   const todoValue = inputField.value;
   showTodoOnTheScreen(todoValue);
+  inputField.value = ""
+}
+
+function clearForm() {
+const inputField = document.querySelector("#todo__input");
+inputField.value = "";
 }
 
 const todoForm = document.getElementById("form");
 todoForm.addEventListener("submit", todoFormSumbitHandler);
+
+const clearButton = document.querySelector("#Clear");
+clearButton.addEventListener("click", clearForm)
